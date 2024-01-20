190db542e2ed2000f50d3aa0ce7aa03c33cbe9a4
kethan39 <106477877+kethan039@users.noreply.github.com>
2024-01-20 16:25:38 +0530

    Update GetStarted.js

--- src/Components/GetStarted/GetStarted.js
+++ src/Components/GetStarted/GetStarted.js
@@ -14,7 +14,7 @@ const GetStarted = () => {
           </span>
 
           <button className="button">
-            <a href="mailto:arfainsabasaba@gmail.com"> Get Started</a>
+            <a href="mailto:kethankumarreddy392002@gmail.com"> Get Started</a>
           </button>
         </div>
       </div>
